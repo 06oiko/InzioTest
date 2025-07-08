a6296a36446ba1d0ec421ac5675519a643e08d3e
Oleksandr Boiko <o_boiko@utb.cz>
2025-07-08 18:11:23 +0200

    +POST request(return .txt with searching results), +requirements, +handle search button

--- app.py
+++ app.py
@@ -1,11 +1,74 @@
-from flask import Flask
+from flask import Flask, request, send_file, jsonify, after_this_request
+import requests
+import os
+from dotenv import load_dotenv
 from flask import render_template
+from datetime import datetime
+import json
+
+
+load_dotenv()
 
 app = Flask(__name__)
+
+API_KEY = os.getenv("API_KEY")
+CX = os.getenv("CX")
 
 @app.route('/')
 def hello():
   return render_template('index.html')
 
+@app.route('/search', methods=['POST'])
+def search():
+  data = request.get_json()
+  query = data.get("query");
+
+  if not query:
+    return jsonify({"error": "Missing query"}), 400
+  
+  params = {
+    "q": query,
+    "key": API_KEY,
+    "cx": CX,
+    "num": 10,
+    "start": 1
+  }
+
+  response = requests.get("https://www.googleapis.com/customsearch/v1", params=params)
+  if response.status_code != 200:
+    return jsonify( {
+        "error": "Google API error",
+        "details": response.text 
+        }), 500
+
+  data = response.json()
+  items = data.get("items", [])
+
+  if not items:
+    return jsonify({"error": "No results found."}), 404
+  
+  timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+
+  filename = f"results_{timestamp}"
+  txt_filename = f"{filename}.txt"
+
+  with open(txt_filename, "w", encoding="utf-8") as f:
+    for item in items:
+      f.write(f"{item['title']}\n")
+      f.write(f"{item['link']}\n")
+      f.write(f"{item.get('snippet', '')}\n")
+      f.write("\n" + "-" * 40 + "\n\n")
+
+  @after_this_request
+  def remove_file(response):
+        try:
+            os.remove(txt_filename)
+        except Exception as e:
+            app.logger.warning(f"File doesn't exist: {e}")
+        return response
+
+  return send_file(txt_filename, as_attachment=True)
+
 if __name__ == '__main__':
   app.run(debug=True)
+  
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -9,5 +9,22 @@ document.addEventListener("DOMContentLoaded", () => {
       alert("Please type a request.");
       return;
     }
+
+    await SearchPost(query);
   });
 });
+
+async function SearchPost(query) {
+  const response = await fetch("/search", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ query }),
+  });
+
+  const blob = await response.blob();
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "search_results.txt";
+  a.click();
+}
